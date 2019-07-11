8c2e90c81def9e14c5d2c2df3c3b0b1625c95ca9
jmuller1984 <joris.muller@kisio.org>
2019-07-11 09:22:06 +0200

    Replace ip ADC2 or ADC1 by VIP ADC

--- fabfile/env/platforms.py
+++ fabfile/env/platforms.py
@@ -145,7 +145,9 @@ env.KRAKEN_START_PORT = 30000
 
 env.AT_BASE_LOGDIR = '/var/log/connectors-rt'
 
-env.ADC_HOSTNAME = 'pa4-adc2-prd.canaltp.prod'
+# VIP ADC
+
+env.ADC_HOSTNAME = '10.93.4.1'
 
 """ default environment variables
     /!\ To overwrite these defaults, define it in the corresponding env below
